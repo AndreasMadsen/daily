366ee6ae5fa3d38a3ea57696bf3739b661f2b239
Andreas Madsen <amwebdk@gmail.com>
2014-01-04 18:09:25 +0100

    [test] reduce tests using the default port

--- test/simple/server-new-optional.js
+++ test/simple/server-new-optional.js
@@ -3,18 +3,17 @@ var path = require('path');
 var test = require('tap').test;
 var DailyServer = require('../../daily.js').Server;
 
-var DEFAULT_PORT = require('../default-port.js');
 var DB_PATH = path.resolve(__dirname, '../temp.db');
 
 test('new keyword is optional', function (t) {
   var server = DailyServer(DB_PATH);
-      server.listen();
+      server.listen(0, '127.0.0.1');
 
   server.once('listening', function () {
     t.deepEqual(server.address(), {
-      address: '0.0.0.0',
+      address: '127.0.0.1',
       family: 'IPv4',
-      port: DEFAULT_PORT
+      port: server.address().port // flexible
     });
 
     server.close(t.end.bind(t));
